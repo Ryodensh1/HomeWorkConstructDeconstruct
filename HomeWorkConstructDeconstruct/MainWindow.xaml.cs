29a7ca4c9ca6e93a5fedda0179734c780dc90276
ryode <ryode@DESKTOP-CJIPEKG>
2024-11-30 11:18:46 +0500

    Добавьте файлы проекта.

---
+++ HomeWorkConstructDeconstruct/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HomeWorkConstructDeconstruct
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Product> products = new List<Product>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddProduct(object sender, RoutedEventArgs e)
+        {
+            var product = new Product("Sample Product", 19.99m, "This is a sample product.");
+            products.Add(product);
+            ProductListBox.Items.Add(product.Name);
+        }
+
+        private void ShowProductDetails(object sender, RoutedEventArgs e)
+        {
+            if (ProductListBox.SelectedItem is null)
+            {
+                return;
+            }
+            var selectedProductName = ProductListBox.SelectedItem.ToString();
+            var product = products.FirstOrDefault(p => p.Name == selectedProductName);
+
+            if (product != null)
+            {
+                var (name, price, description) = product;
+                MessageBox.Show($"Name: {name}\nPrice: {price}\nDescription: {description}", "Product Details");
+            }
+        }
+    }
+
+
+}
---
+++ HomeWorkConstructDeconstruct/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HomeWorkConstructDeconstruct
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        // Конструктор по умолчанию
+        public Product() { }
+
+        // Конструктор с параметрами
+        public Product(string name, decimal price, string description)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+        }
+
+        // Деконструктор
+        public void Deconstruct(out string name, out decimal price, out string description)
+        {
+            name = Name;
+            price = Price;
+            description = Description;
+        }
+
+    }
+    
+
+
+}
